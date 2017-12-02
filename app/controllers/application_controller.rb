6ece5cec25498174db0a057a13ce8dd3802ae09e
SenseyAD <senseyad@icloud.com>
2017-12-02 11:42:23 +0200

    Update Gemfike for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello, wwwworld!"
+    render html: "Hello, World!"
   end
 end
